682edbaa4a1f3bd6fa42c149cfa034c0212e1ea2
liantai <liantai@juanpi.com>
2016-02-18 23:44:09 +0800

    提交错误，删除host文件

--- src/test/resources/com/dessert/command/switchhost/switchTo171.bat
+++
@@ -1,4 +0,0 @@
-@echo off 
-IF NOT EXIST C:\Windows\System32\drivers\etc\hosts.bak COPY /y hosts C:\Windows\System32\drivers\etc\hosts.bak
-DEL hosts
-COPY /y .\myhosts\171.hosts C:\Windows\System32\drivers\etc\hosts
